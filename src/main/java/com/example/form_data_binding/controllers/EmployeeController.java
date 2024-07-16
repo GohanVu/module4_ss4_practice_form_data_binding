a1bab83ec5f84cf59d6f11fca028900bb575e46c
GohanVu <hoangvhdata@gmail.com>
2024-07-16 14:26:41 +0700

    update

---
+++ src/main/java/com/example/form_data_binding/controllers/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.example.form_data_binding.controllers;
+
+import com.example.form_data_binding.models.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+    @RequestMapping(value = "/showForm", method = RequestMethod.GET)
+    public String showForm(ModelMap modelMap){
+        modelMap.addAttribute("employee", new Employee());
+        return "employee/create";
+    }
+
+    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+    public String submit (@ModelAttribute ("employee") Employee employee, ModelMap modelMap){
+        modelMap.addAttribute("name",employee.getName());
+        modelMap.addAttribute("contactNumber", employee.getContactNumber());
+        modelMap.addAttribute("id",employee.getId());
+        return "employee/info";
+    }
+}
